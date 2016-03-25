85dc2a8d40dcbcf1fe1f5cf70a579a41340af579
Xu Wei <Panblack@users.noreply.github.com>
2016-03-25 16:41:45 +0800

    Update fin.sh

--- ezdpl/zabbix/fin.sh
+++ ezdpl/zabbix/fin.sh
@@ -8,7 +8,7 @@ sed -i 's/ServerActive=127.0.0.1/ServerActive=10.60.1.254/g' /etc/zabbix/zabbix_
 sed -i "s/Hostname=Zabbix server/Hostname=$_hostname/g" /etc/zabbix/zabbix_agentd.conf
 
 if ! iptables -nL |grep 10050|grep ACCEPT; then
-    _eth=`ip a |grep 10.60.1 -B2|head -1 |awk -F: '{print $2}'`
+    _eth=`ip a |grep 10.1.1 -B2|head -1 |awk -F: '{print $2}'`
     _num=`iptables -nL --line-number|grep REJECT|awk '{print $1}'|head -1`
     iptables -I INPUT $_num -i $_eth -p tcp -m state --state NEW -m tcp --dport 10050 -j ACCEPT
     /etc/init.d/iptables save
