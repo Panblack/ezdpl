6122e0391b6b158711c6413a29f2909eee6f9408
xw <panblack@126.com>
2016-08-11 16:11:20 +0800

    bugfix(similar server names cause script failure.

--- ezdpl/haproxy/backend-cfg-switch.sh
+++ ezdpl/haproxy/backend-cfg-switch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# backend server names in haproxy.cfg must be unique.
 # Backup haproxy.cfg
 # Comment/Uncomment specific server-name in backend section
 # Reload haproxy service.
@@ -9,10 +10,10 @@ _cfg_file="/etc/haproxy/haproxy.cfg"
 _log_file="/opt/haproxy_switch_log/switch.log"
 _backend_server=$1
 _oper=$2
-_match=`grep "server.*$_backend_server" $_cfg_file`
+_match=`grep -w "server.*$_backend_server" $_cfg_file`
 set -e
 set -u
-echo -en "$_time\t$_backend_server\t$_oper" >> $_log_file
+echo -en "$_time\tCFG\t$_backend_server\t$_oper" >> $_log_file
 
 /bin/cp $_cfg_file /opt/haproxy_switch_log/haproxy.cfg.$_time
 case $_oper in
--- ezdpl/haproxy/backend-switch.sh
+++ ezdpl/haproxy/backend-switch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# Backup haproxy.cfg
+# backend server names in haproxy.cfg must be unique.
 # find matched server ip and port
+#    server appserver012 10.0.1.75:80 check
+#    server webserver028 10.0.1.93:8080 check
 # iptables -A OUTPUT -d <ip> -p tcp --dport <port> -j DORP
 # OR 
 # iptables -D OUTPUT -d <ip> -p tcp --dport <port> -j DORP
@@ -25,8 +27,8 @@ if [[ -z $_backend_server ]] || [[ -z $_oper ]]; then
     exit 0
 fi
     
-echo -en "$_time\t$_backend_server\t$_oper\t" >> $_log_file
-_match=`grep "server.*$_backend_server" $_cfg_file|awk '{print $3}'`
+echo -en "$_time\tNET\t$_backend_server\t$_oper\t" >> $_log_file
+_match=`grep -w "server.*$_backend_server" $_cfg_file|awk '{print $3}'`
 if [[ -z $_match ]]; then
    echo "backend server does not exist." | tee -a $_log_file
    exit 1
