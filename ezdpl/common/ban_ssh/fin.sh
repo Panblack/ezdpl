e45f54f27e7046fabd38ea3dc8e8b4b25b05cc71
Xu Wei <panblack@126.com>
2017-03-25 20:14:35 +0800

    clear 1.0

--- ezdpl/common/ban_ssh/fin.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-_cron="*  */1 * * * /usr/local/bin/ban_ssh.sh"
-sed -i /"ban_ssh"/d /var/spool/cron/root
-echo "$_cron" >> /var/spool/cron/root
--- ezdpl/lvs/Director/kawatch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Print realtime keepalived VRRP Multicast Info.
-tcpdump -vvv -n -i eth0 dst 224.0.0.18
