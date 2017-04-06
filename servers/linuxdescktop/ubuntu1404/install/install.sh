2935f4d30240b12dd24e9759be9af262a57f2ae2
xuw <panblack@126.com>
2017-04-06 14:06:13 +0800

    backend-switch bugfix:port identification error; ubuntu1404/install.sh bugfix

--- servers/linuxdescktop/ubuntu1404/install/install.sh
+++ servers/linuxdescktop/ubuntu1404/install/install.sh
@@ -9,7 +9,7 @@ echo "Upgrade & install necessary packages"
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y gnome-tweak-tool unity-tweak-tool sysv-rc-conf dconf-editor \
-leafpad tree p7zip-full p7zip-rar telnet bash ssh vim nmap lynx iftop iptraf convmv sysstat dstat curl httping xclip \
+leafpad tree p7zip-full p7zip-rar telnet ssh vim nmap lynx iftop iptraf convmv enca sysstat dstat curl httping xclip \
 smplayer vlc vlc-* gstreamer1.0-plugins* openshot gimp gthumb graphicsmagick kazam gaupol ttf-wqy-microhei \
 git meld subversion \
 psensor indicator-cpufreq \
@@ -38,11 +38,11 @@ sudo chown -R PANBLACK:PANBLACK /home/app
 #sudo cnpm install -g whistle
 echo "nodejs & wps,virtualbox (2017-03)"
 cd /home/app/iso/packages
-wget https://atom-installer.github.com/v1.15.0/atom-amd64.deb
-wget https://nodejs.org/dist/v6.10.0/node-v6.10.0-linux-x64.tar.xz
-wget http://kdl.cc.ksosoft.com/wps-community/download/a21/wps-office_10.1.0.5672~a21_amd64.deb
-wget http://download.virtualbox.org/virtualbox/5.1.16/virtualbox-5.1_5.1.16-113841~Ubuntu~trusty_amd64.deb
-wget http://download.virtualbox.org/virtualbox/5.1.16/Oracle_VM_VirtualBox_Extension_Pack-5.1.16-113841.vbox-extpack
+wget -N https://atom-installer.github.com/v1.15.0/atom-amd64.deb
+wget -N https://nodejs.org/dist/v6.10.0/node-v6.10.0-linux-x64.tar.xz
+wget -N http://kdl.cc.ksosoft.com/wps-community/download/a21/wps-office_10.1.0.5672~a21_amd64.deb
+wget -N http://download.virtualbox.org/virtualbox/5.1.16/virtualbox-5.1_5.1.16-113841~Ubuntu~trusty_amd64.deb
+wget -N http://download.virtualbox.org/virtualbox/5.1.16/Oracle_VM_VirtualBox_Extension_Pack-5.1.16-113841.vbox-extpack
 
 cd /home/app
 tar Jxvf /home/app/iso/packages/node-v6.10.0-linux-x64.tar.xz && ln -sf /home/app/node-v6.10.0-linux-x64 /home/app/node
@@ -50,7 +50,8 @@ echo
 
 echo "Local Configuration"
 cd $_pwd
-/bin/cp -p ./packages/*.deb /home/app/iso/packages/ 2>/dev/null
+/bin/cp -p ./deb/*.deb /home/app/iso/packages/ 2>/dev/null
+/bin/cp -p ./scripts/* /usr/local/bin/ 2>/dev/null
 echo 
 
 echo "Modifying ~/.bashrc"
@@ -73,24 +74,6 @@ sudo /bin/cp zz_custom_env.sh /etc/profile.d/
 source /etc/profile
 echo 
 
-# ufw settings
-echo "ufw"
-sudo ufw enable
-sudo ufw default deny
-sudo ufw allow 80/tcp
-sudo ufw allow 8080/tcp
-sudo ufw allow 1080/tcp
-#sudo ufw allow 443/tcp
-#sudo ufw allow 3306/tcp
-#sudo ufw allow 10001/tcp
-#sudo ufw allow 10002/tcp
-echo
-
-echo "Blocking wo.com.cn"
-sudo /bin/cp -p /etc/ufw/before.rules /etc/ufw/before.rules.`date +%F_%H%M`
-sudo sed -i "/End required lines/r blocking.wo.com.cn" /etc/ufw/before.rules
-echo 
-
 echo "Installing node packages"
 sudo npm install -g whistle
 sudo npm install -g anyproxy
@@ -109,6 +92,31 @@ sudo chmod u+s /usr/bin/dumpcap
 echo "Make virtualbox VM's able to connect USB devices."
 sudo usermod -aG vboxusers PANBLACK
 
+# ufw settings
+echo "ufw"
+sudo ufw enable
+sudo ufw default deny
+sudo ufw allow 80/tcp
+sudo ufw allow 8080/tcp
+sudo ufw allow 1080/tcp
+#sudo ufw allow 443/tcp
+#sudo ufw allow 3306/tcp
+#sudo ufw allow 10001/tcp
+#sudo ufw allow 10002/tcp
+echo
+
+echo "Blocking wo.com.cn"
+sudo /bin/cp -p /etc/ufw/before.rules /etc/ufw/before.rules.`date +%F_%H%M`
+_IFS=$IFS
+IFS="
+"
+for x in `cat blocking.wo.com.cn`; do
+    sed -i /"$x"/d /etc/ufw/before.rules
+done
+sudo sed -i "/End required lines/r blocking.wo.com.cn" /etc/ufw/before.rules
+IFS=$_IFS
+echo 
+
 echo "Disable Embed preedit text ..."
 ibus-setup
 
@@ -117,7 +125,7 @@ dconf-editor
 
 # Theme tweak
 sudo /bin/cp -p /usr/share/themes/Ambiance/gtk-3.0/gtk-widgets.css /usr/share/themes/Ambiance/gtk-3.0/gtk-widgets.css.bak
-sudo sed  '1212s/bg_color,/selected_bg_color,/' /usr/share/themes/Ambiance/gtk-3.0/gtk-widgets.css
+sudo sed -i '1212s/bg_color,/selected_bg_color,/' /usr/share/themes/Ambiance/gtk-3.0/gtk-widgets.css
 #":1212	background-color: shade (@bg_color, 1.02);"
 
 cd
@@ -137,3 +145,7 @@ exit 0
 # calibre sound-juicer isomaster fcitx-googlepinyin 
 # discontinued packages
 # chmsee gnome-subtitles(replaced with Gaupol)
+
+# Blocking wo.com.cn 
+#for x in `seq 1 9`; do nslookup lndnserror$x.wo.com.cn|grep "Address: "|sort|uniq|sed 's/Address: //g'; done
+#for x in `seq 1 9`; do nslookup jldnserror$x.wo.com.cn|grep "Address: "|sort|uniq|sed 's/Address: //g'; done
