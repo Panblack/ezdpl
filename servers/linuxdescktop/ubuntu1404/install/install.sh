fac9a0fb98f855280c9c606206a3e52ddcdae939
xuw <panblack@126.com>
2017-04-07 20:29:01 +0800

    ubuntu1404 scripts debugged

--- servers/linuxdescktop/ubuntu1404/install/install.sh
+++ servers/linuxdescktop/ubuntu1404/install/install.sh
@@ -4,67 +4,36 @@
 # ** Replace 'PANBLACK' with your username **
 #
 
-_pwd=`pwd`
 echo "Upgrade & install necessary packages"
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y gnome-tweak-tool unity-tweak-tool sysv-rc-conf dconf-editor \
+sudo apt-get install -y -m gnome-tweak-tool unity-tweak-tool sysv-rc-conf dconf-editor \
 leafpad tree p7zip-full p7zip-rar telnet ssh vim nmap lynx iftop iptraf convmv enca sysstat dstat curl httping xclip \
+git meld subversion chromium-browser libsdl1.2debian libqt4-opengl python-pip \
 smplayer vlc vlc-* gstreamer1.0-plugins* openshot gimp gthumb graphicsmagick kazam gaupol ttf-wqy-microhei \
-git meld subversion \
 psensor indicator-cpufreq \
 rdesktop virt-manager virt-viewer wireshark lua5.2 lua-bitop \
-chromium-browser libqt4-opengl \
-docker.io apache2 php5 apache2-utils python-pip \
+docker.io apache2 php5 apache2-utils cgroup-bin \
 openvpn network-manager-openvpn network-manager-openvpn-gnome 
+if [[ $? != 0 ]];then
+    echo "Exit!"
+    exit 100
+fi
 echo 
 
-# python pip & tools
-echo "pip install memcached-cli, httpie"
-sudo pip install --upgrade pip
-sudo pip install memcached-cli
-sudo pip install httpie
-echo
-
-# app dir
-echo "/home/app/iso/packages"
-sudo mkdir -p /home/app/iso/packages
-sudo chown -R PANBLACK:PANBLACK /home/app
-
-# node.js , whistle
-#sudo curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-#sudo apt-get install node
-#sudo npm install cnpm -g --registry=https://registry.npm.taobao.org
-#sudo cnpm install -g whistle
-echo "nodejs & wps,virtualbox (2017-03)"
-cd /home/app/iso/packages
-wget -N https://atom-installer.github.com/v1.15.0/atom-amd64.deb
-wget -N https://nodejs.org/dist/v6.10.0/node-v6.10.0-linux-x64.tar.xz
-wget -N http://kdl.cc.ksosoft.com/wps-community/download/a21/wps-office_10.1.0.5672~a21_amd64.deb
-wget -N http://download.virtualbox.org/virtualbox/5.1.16/virtualbox-5.1_5.1.16-113841~Ubuntu~trusty_amd64.deb
-wget -N http://download.virtualbox.org/virtualbox/5.1.16/Oracle_VM_VirtualBox_Extension_Pack-5.1.16-113841.vbox-extpack
-
-cd /home/app
-tar Jxvf /home/app/iso/packages/node-v6.10.0-linux-x64.tar.xz && ln -sf /home/app/node-v6.10.0-linux-x64 /home/app/node
-echo 
-
+_pwd=$(dirname `readlink -f $0`)
+echo $_pwd
 echo "Local Configuration"
-cd $_pwd
-/bin/cp -p ./deb/*.deb /home/app/iso/packages/ 2>/dev/null
-/bin/cp -p ./scripts/* /usr/local/bin/ 2>/dev/null
-echo 
-
-echo "Modifying ~/.bashrc"
-/bin/cp -p ~/.bashrc ~/.bashrc.bak.`date +%F_%H%M`
-sed -i /'alias ll'/d ~/.bashrc
-sed -i 's/]:/] \\t /g' ~/.bashrc
-sed -i 's/^#force_color_prompt=yes/force_color_prompt=yes/g' ~/.bashrc
-/bin/cp vimrc ~/.vimrc
+sudo bin/cp -p ./scripts/* /usr/local/bin/ 2>/dev/null
+/bin/cp     -p ./deb/*.deb /home/app/iso/packages/ 2>/dev/null
 echo 
 
 echo "Modifying /etc/skel/.bashrc"
+cp ~/.bash_aliases ~/.bash_aliases.`date +%F_%H%M`
+/bin/cp bash_aliases ~/.bash_aliases
+sudo /bin/cp bash_aliases /etc/skel/.bash_aliases
 sudo /bin/cp -p /etc/skel/.bashrc /etc/skel/.bashrc.bak.`date +%F_%H%M`
-sudo sed -i /alias ll/d /etc/skel/.bashrc
+sudo sed -i /"alias ll"/d /etc/skel/.bashrc
 sudo sed -i 's/]:/] \\t /g' /etc/skel/.bashrc
 sudo sed -i 's/^#force_color_prompt=yes/force_color_prompt=yes/g' /etc/skel/.bashrc
 echo 
@@ -74,23 +43,10 @@ sudo /bin/cp zz_custom_env.sh /etc/profile.d/
 source /etc/profile
 echo 
 
-echo "Installing node packages"
-sudo npm install -g whistle
-sudo npm install -g anyproxy
-echo 
-
-# Install deb packages
-cd /home/app/iso/packages/
-for x in *.deb; do sudo dpkg -i $x; done
-
-echo "Disable dash"
-sudo dpkg-reconfigure dash
-
-echo "Make wireshark able to capture packets with non-root user."
-sudo chmod u+s /usr/bin/dumpcap
-
-echo "Make virtualbox VM's able to connect USB devices."
-sudo usermod -aG vboxusers PANBLACK
+echo "Theme tweak"
+sudo /bin/cp -p /usr/share/themes/Ambiance/gtk-3.0/gtk-widgets.css /usr/share/themes/Ambiance/gtk-3.0/gtk-widgets.css.bak
+sudo sed -i '1212s/bg_color,/selected_bg_color,/' /usr/share/themes/Ambiance/gtk-3.0/gtk-widgets.css
+#":1212	background-color: shade (@bg_color, 1.02);"
 
 # ufw settings
 echo "ufw"
@@ -111,22 +67,80 @@ _IFS=$IFS
 IFS="
 "
 for x in `cat blocking.wo.com.cn`; do
-    sed -i /"$x"/d /etc/ufw/before.rules
+    sudo sed -i /"$x"/d /etc/ufw/before.rules
 done
 sudo sed -i "/End required lines/r blocking.wo.com.cn" /etc/ufw/before.rules
 IFS=$_IFS
 echo 
 
+echo "Modifying ~/.bashrc"
+/bin/cp -p ~/.bashrc ~/.bashrc.bak.`date +%F_%H%M`
+sed -i /'alias ll'/d ~/.bashrc
+sed -i 's/]:/] \\t /g' ~/.bashrc
+sed -i 's/^#force_color_prompt=yes/force_color_prompt=yes/g' ~/.bashrc
+echo 
+
+echo "App dir: /home/app"
+sudo mkdir -p /home/app/iso/packages
+sudo chown -R PANBLACK:PANBLACK /home/app
+
+
+echo "vimrc.local"
+sudo /bin/cp vimrc /etc/vim/vimrc.local
+
+# python pip & tools
+echo "pip install memcached-cli, httpie"
+sudo pip install --upgrade pip
+sudo pip install memcached-cli
+sudo pip install httpie
+echo
+
+# node.js , whistle
+#sudo curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+#sudo apt-get install node
+#npm install cnpm -g --registry=https://registry.npm.taobao.org
+#cnpm install -g whistle
+echo "nodejs & wps,virtualbox (2017-03)"
+cd /home/app/iso/packages
+wget -N https://atom-installer.github.com/v1.15.0/atom-amd64.deb
+wget -N https://nodejs.org/dist/v6.10.0/node-v6.10.0-linux-x64.tar.xz
+wget -N http://kdl.cc.ksosoft.com/wps-community/download/a21/wps-office_10.1.0.5672~a21_amd64.deb
+wget -N http://download.virtualbox.org/virtualbox/5.1.16/virtualbox-5.1_5.1.16-113841~Ubuntu~trusty_amd64.deb
+wget -N http://download.virtualbox.org/virtualbox/5.1.16/Oracle_VM_VirtualBox_Extension_Pack-5.1.16-113841.vbox-extpack
+
+cd /home/app
+tar Jxvf /home/app/iso/packages/node-v6.10.0-linux-x64.tar.xz && ln -sf /home/app/node-v6.10.0-linux-x64 /home/app/node
+echo 
+
+echo "Installing node packages"
+export PATH=$PATH:/home/app/node/bin/
+npm install -g whistle
+npm install -g anyproxy
+echo 
+
+echo "Installing deb packages"
+cd /home/app/iso/packages/
+for x in *.deb; do sudo dpkg -i $x; done
+echo 
+
+echo "Make wireshark able to capture packets with non-root user."
+sudo chmod u+s /usr/bin/dumpcap
+echo
+
+echo "Make virtualbox VM's able to connect USB devices."
+sudo usermod -aG vboxusers PANBLACK
+echo 
+
+echo "Disable dash"
+sudo dpkg-reconfigure dash
+echo 
+
 echo "Disable Embed preedit text ..."
 ibus-setup
+echo 
 
 echo "Modify org.gnome.gnome-screenshot"
 dconf-editor
-
-# Theme tweak
-sudo /bin/cp -p /usr/share/themes/Ambiance/gtk-3.0/gtk-widgets.css /usr/share/themes/Ambiance/gtk-3.0/gtk-widgets.css.bak
-sudo sed -i '1212s/bg_color,/selected_bg_color,/' /usr/share/themes/Ambiance/gtk-3.0/gtk-widgets.css
-#":1212	background-color: shade (@bg_color, 1.02);"
 
 cd
 
--- servers/linuxdescktop/ubuntu1404/install/zz_custom_env.sh
+++ servers/linuxdescktop/ubuntu1404/install/zz_custom_env.sh
@@ -1,18 +1,3 @@
-alias cp='/bin/cp -i --preserve=timestamps'
-alias mv='mv -i'
-alias rm='rm -i'
-alias df='/bin/df -hTP'
-alias du='/usr/bin/du -hx --max-depth=1'
-alias ll='/bin/ls -lh --color=auto --time-style=long-iso 2>/dev/null'
-alias lt='/bin/ls -lhtr --color=auto --time-style=long-iso 2>/dev/null'
-alias mysql='mysql --default-character-set=utf8'
-alias tless='less `ls -tr1|tail -1`'
-alias ttail='tail -f `ls -tr1|tail -1`'
-alias xscp='/usr/bin/scp -P2139 '
-alias xssh='/usr/bin/ssh -p2139 '
-alias ping='ping -i 0.2 '
-alias lynx='lynx -accept_all_cookies '
-
 export JAVA_HOME=/home/app/jdk
 export JRE_HOME=$JAVA_HOME/jre
 export CLASSPATH=$CLASSPATH:.:$JAVA_HOME/lib:$JAVA_HOME/jre/lib
