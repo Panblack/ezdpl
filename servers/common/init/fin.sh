3f642f47461bd1e3421044c5b67138628d9dcba4
xuw <panblack@126.com>
2017-06-01 09:37:06 +0800

    common/init/fin.s: aliyun server detect rules fixed.

--- servers/common/init/fin.sh
+++ servers/common/init/fin.sh
@@ -64,8 +64,8 @@ esac
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 setenforce 0
 
-# aliyun 
-if  grep "mirrors.cloud.aliyuncs.com" yum.repos.d/CentOS-Base.repo &>/dev/null ; then
+# aliyun (exclude kernel centos-release when `yum update`) 
+if egrep "(mirrors.aliyuncs.com|mirrors.aliyun.com)" /etc/yum.repos.d/CentOS-Base.repo &>/dev/null ; then
     cp -p /etc/yum.conf /etc/yum.conf.bak
     echo "exclude=kernel* centos-release*" >> /etc/yum.conf
 fi
