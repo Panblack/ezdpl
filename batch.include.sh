9448f21bb65fb77a1acf77c7a244d45ded940948
Panblack <panblack@126.com>
2017-07-13 11:05:06 +0800

    batch.include.sh sample

--- batch.include.sh
+++ batch.include.sh
@@ -1,6 +1,7 @@
-#scon $_host e "cat /etc/*-release"
-#ssh -p$_port root@$_ip 'yum install -y bash openssl'
+#./ezdpl Y $_ip:$_port    common/zabbix
+#scon $_host e            "cat /etc/*-release"
+#scs $_user@$_ip:$_port   'nst a |grep ESTAB'
+#ssh -p$_port $_user@$_ip 'yum install -y bash openssl'
 #scp -p ./servers/javasrv/init/files/usr/local/bin/tmc $_user@$_ip:/usr/local/bin/
-#./ezdpl Y $_ip:$_port common/zabbix
-ssh -p$_port root@$_ip 'uname -a'
+#scp -p $_user@$_ip:/root/sample ~/sample
 
