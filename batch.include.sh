c50292baef9a09550bab972f8c87592b3e5f4b55
Panblack <panblack@126.com>
2017-07-13 10:41:12 +0800

    ezdpl-2.0.mysql updated, batch.sh supports both mysql and hosts.lst

--- batch.include.sh
+++ batch.include.sh
@@ -1,4 +1,6 @@
 #scon $_host e "cat /etc/*-release"
-#ssh -p$_port root@$_host 'yum install -y bash openssl'
-#scp -p ./servers/javasrv/init/files/usr/local/bin/tmc root@$_host:/usr/local/bin/
-#./ezdpl Y $_host:$_port common/zabbix
+#ssh -p$_port root@$_ip 'yum install -y bash openssl'
+#scp -p ./servers/javasrv/init/files/usr/local/bin/tmc $_user@$_ip:/usr/local/bin/
+#./ezdpl Y $_ip:$_port common/zabbix
+ssh -p$_port root@$_ip 'uname -a'
+
--- batch.sh
+++ batch.sh
@@ -1,41 +1,44 @@
 #!/bin/bash
-source ./batch.where.sh
-case $1 in
-    web)
-	_SQL=" SELECT srvname , port , webname FROM v_srvweb $_where "
-	;;
-    all)
-	_SQL=" SELECT name , port , purpose FROM srv $_where "
-	;;
-    *)
-	echo "Usage: batch.sh <web|all>"
-	exit 0
-	;;
-esac
-_servers=`/opt/ezdpl/bin/sqlezdpl "$_SQL" 2>/dev/null |egrep -v 'name'`
+# Determine ezdpl home
+_dir=$(dirname `readlink -f $0`)
+cd $_dir
+echo "ezdplHome:`pwd`"
+
+# Read servers
+if [[ -f ./hosts.lst ]]; then
+    _servers=`egrep -v '(^#|^$)' ./hosts.lst`
+else
+    source ./batch.where.sh
+    _SQL=" SELECT  ip , name , user , port , purpose FROM srv $_where "
+    _servers=`./bin/sqlezdpl "$_SQL" 2>/dev/null |egrep -v 'purpose'`
+fi
+
+# Confirm
 echo "$_servers"
+echo 
+echo "Where:"
+egrep -v '(^#|^$)' ./batch.where.sh
 echo 
 echo "Commands:"
 egrep -v '^ *#' ./batch.include.sh
 echo
-echo "Where:"
-cat ./batch.where.sh
-echo 
 read -p "Press Y to continue:" _go
 if [[ $_go != Y ]]; then
     exit 0
 fi
 
+# Start batch jobs
 _count=1
 IFS=$'\n'
 for x in $_servers ; do
-  _host=`echo $x|awk -F"\t" '{print $1}'`
-  _port=`echo $x|awk -F"\t" '{print $2}'`
-  _purpose=` echo $x|awk -F"\t" '{print $3}'`
-
-  echo "$_count [ $_host : $_port : $_purpose ]"
-  source ./batch.include.sh
-  echo
-  ((_count++))
+    _ip=`  echo $x|awk '{print $1}'`
+    _host=`echo $x|awk '{print $2}'`
+    _user=`echo $x|awk '{print $3}'`
+    _port=`echo $x|awk '{print $4}'`
+    _purpose=`echo $x|awk '{print $5}'`
+    echo "$_count [ $_ip , $_user@$_host:$_port , $_purpose ]"
+    source ./batch.include.sh
+    echo
+    ((_count++))
 done
 exit 0
