3c27bcaad70d7248e232f752fb8809b71d849624
Panblack <panblack@126.com>
2017-11-07 17:34:35 +0800

    mysql/init/fin.sh BUGFIX , rabbitmq/init/fin.sh user inits

--- servers/mysql/init/fin.sh
+++ servers/mysql/init/fin.sh
@@ -8,28 +8,33 @@ mkdir -p /data/backup
 
 case $_RELEASE in
     CENTOS6|CENTOS7)
+	rpmkeys --import https://repo.mysql.com/RPM-GPG-KEY-mysql
 	if [[ $_RELEASE = CENTOS6 ]]; then
     	    rpm -ivh https://repo.mysql.com/mysql57-community-release-el6.rpm
         elif [[ $_RELEASE = CENTOS7 ]]; then
     	    rpm -ivh https://repo.mysql.com/mysql57-community-release-el7.rpm
 	fi
-	read -p "Now edit mysql repo file....[Enter]"
-	if [[ -f /etc/yum.repos.d/mysql-community.repo ]]; then
-    	    vim /etc/yum.repos.d/mysql-community.repo
-	fi
 
-	for x in `ps aux|grep -i 'yum' |grep -v grep|awk '{print $2}'`; do
+	for x in `ps aux|grep -i 'yum' |grep -v grep|awk '{print $2}'`; do 
     		kill $x || kill -9 $x
 	done
 	yum clean all
-	rpmkeys --import https://repo.mysql.com/RPM-GPG-KEY-mysql
-	echo "Installing mysql-server..."
+	yum install -y yum-utils
+	yum-config-manager --quiet --disable mysql57-community > /dev/null
+	yum-config-manager --quiet --disable mysql80-community > /dev/null
+	yum-config-manager --quiet --enable  mysql56-community > /dev/null
+	grep "enabled=1" -B2 /etc/yum.repos.d/mysql-community.repo
+	echo;echo 
+	echo "Installing mysql-community-server..."
 	yum install -y mysql-community-server
 	mv /etc/my.cnf files/etc/my.cnf.org
 	mv /etc/my.cnf.new /etc/my.cnf
 	chkconfig mysqld on
-	service start mysqld
-	mysql_secure_installation
+	service mysqld start
+	echo;echo
+	echo "Login remote server and run:"
+       	echo "mysql_secure_installation"
+	echo "mysql_config_editor set --host=localhost --user=root --password"
 	;;
     UBUNTU)
 	sudo dpkg -i https://repo.mysql.com/mysql-apt-config_0.8.7-1_all.deb
--- servers/rabbitmq/init/fin.sh
+++ servers/rabbitmq/init/fin.sh
@@ -8,5 +8,11 @@ rpm --import https://www.rabbitmq.com/rabbitmq-release-signing-key.asc
 yum install -y rabbitmq-server-3.6.9-1.el7.noarch.rpm
 systemctl enable rabbitmq-server.service
 systemctl start rabbitmq-server.service
+rabbitmqctl add_user rmquser user_pass
+rabbitmqctl add_user rmqadmin admin_pass
+rabbitmqctl set_user_tags rmqadmin administrator
+rabbitmqctl set_user_tags guest
+rabbitmqctl set_permissions -p / rmquser ".*" ".*" ".*"
+rabbitmqctl start_app
 
 echo "`date +%F_%T` rabbitmq/init " >> /tmp/ezdpl.log
