4295cb4a5c34cb0d8334c3dc978b9baf21f31fec
panblack <panblack@126.com>
2018-11-20 10:46:55 +0800

    compress-log.sh:verify dir & days

--- servers/common/init/files/usr/local/bin/compress-log.sh
+++ servers/common/init/files/usr/local/bin/compress-log.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-_dir=$1
-_days=$2
-if [[ -z $_dir ]] && [[ -z $_days ]]; then
+if [[ $# -lt 2 ]]; then
     echo "compress-log.sh <dir> <days before>"
     exit 1
 fi
-cd $_dir
+_dir=$1
+_days=$2
+if [[ ! -d $_dir ]]; then
+    echo "$_dir does not exist";exit 1
+fi
+if [[ $_days -le 0 ]]; then
+    echo "$_days <= 0";exit 1
+fi
 for x in `find -type f -mtime +${_days}|egrep -v "\.gz$"`; do
     if [[ `stat --format=%s ${x}` != 0 ]]; then
         gzip ${x}
