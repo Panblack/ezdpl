87abab0e846e857c605044c7deac5dd66b848cc1
panblack <panblack@126.com>
2018-11-23 16:53:44 +0800

    servers/common/firewall,zabbix updated

--- servers/common/firewall/pre.sh
+++ servers/common/firewall/pre.sh
@@ -3,36 +3,37 @@
 source /usr/local/bin/release.include
 echo $_RELEASE
 
-_FIREWALL_PORTS
-# firewalld 
-case $_RELEASE in
-    CENTOS6)
-        # firewall
-        chkconfig iptables on
-        service iptables start
-        iptables -D INPUT -j REJECT --reject-with icmp-host-prohibited
-	for x in $_FIREWALL_PORTS; do
-            iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport $x -j ACCEPT
-	done
-        iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
-        /etc/init.d/iptables save
-        ;;
-    CENTOS7)
-        # firewall
-        systemctl enable firewalld
-        systemctl start  firewalld
-	for x in $_FIREWALL_PORTS; do 
-	    firewall-cmd --add-port $x/tcp --permanent
-        done
-        firewall-cmd --reload
-        ;;
-    UBUNTU)
-        # firewall
-        sudo ufw enable
-        sudo ufw default deny
-	for x in $_FIREWALL_PORTS; do
-            sudo ufw allow $x/tcp
-	done
-        ;;
-esac
+/usr/local/bin/iptables-iport a "$_FIREWALL_PORTS"
 echo "`date +%F_%T` common/firewall " >> /tmp/ezdpl.log
+exit 
+
+#case $_RELEASE in
+#    CENTOS6)
+#        # firewall
+#        chkconfig iptables on
+#        service iptables start
+#        iptables -D INPUT -j REJECT --reject-with icmp-host-prohibited
+#	for x in $_FIREWALL_PORTS; do
+#            iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport $x -j ACCEPT
+#	done
+#        iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
+#        /etc/init.d/iptables save
+#        ;;
+#    CENTOS7)
+#        # firewall
+#        systemctl enable firewalld
+#        systemctl start  firewalld
+#	for x in $_FIREWALL_PORTS; do 
+#	    firewall-cmd --add-port $x/tcp --permanent
+#        done
+#        firewall-cmd --reload
+#        ;;
+#    UBUNTU)
+#        # firewall
+#        sudo ufw enable
+#        sudo ufw default deny
+#	for x in $_FIREWALL_PORTS; do
+#            sudo ufw allow $x/tcp
+#	done
+#        ;;
+#esac
--- servers/common/zabbix/fin.sh
+++ servers/common/zabbix/fin.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
-rpm -ivh `ls /opt/packages/zabbix-release-*.noarch.rpm`
-killall -9 yum
-yum clean all
-while true; do 
-    yum install zabbix-agent -y
-    if [[ $? = 0 ]];then
-	break
-    fi
-    echo
-    sleep 2
-done
+source /usr/local/bin/release.include
+echo $_RELEASE
+case $_RELEASE in
+    CENTOS6)
+	rpm -ivh https://repo.zabbix.com/zabbix/4.0/rhel/6/x86_64/zabbix-release-4.0-1.el6.noarch.rpm
+	killall -9 yum
+	yum clean all
+	yum install -y zabbix-agent
+	chkconfig zabbix-agent on
+        ;;
+    CENTOS7)
+	rpm -ivh https://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-1.el7.noarch.rpm
+	killall -9 yum
+	yum clean all
+	yum install -y zabbix-agent
+	systemctl enable zabbix-agent
+        ;;
+    UBUNTU)
+	#18.04
+	wget https://repo.zabbix.com/zabbix/4.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_4.0-2+bionic_all.deb
+	dpkg -i zabbix-release_4.0-2+bionic_all.deb
+	apt update
+	apt install -y zabbix-agent
+        ;;
+esac
 
+_server_ip="1.2.3.4"
 _hostname=`hostname -s`
-sed -i 's/Server=127.0.0.1/Server=10.1.1.254/g' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/ServerActive=127.0.0.1/ServerActive=10.1.1.254/g' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/Server=127.0.0.1/Server='$_server_ip'/g' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/ServerActive=127.0.0.1/ServerActive='$_server_ip'/g' /etc/zabbix/zabbix_agentd.conf
 sed -i "s/Hostname=Zabbix server/Hostname=$_hostname/g" /etc/zabbix/zabbix_agentd.conf
 
+/usr/local/bin/iptables-iport a 10050
+
 # add 10050/tcp iptables rule to local connection.
-if ! iptables -nL |grep 10050|grep ACCEPT; then
-    _eth=`ip a |grep "10.1.1" -B2|head -1 |awk -F: '{print $2}'`
-    _num=`iptables -nL --line-number|grep REJECT|awk '{print $1}'|head -1`
-    iptables -I INPUT $_num -i $_eth -p tcp -m state --state NEW -m tcp --dport 10050 -j ACCEPT
-    /etc/init.d/iptables save
-    iptables -nL --line-numbers |grep 10050
-fi
-# iptables-iport is not powerful enough for this. 
+#if ! iptables -nL |grep 10050|grep ACCEPT; then
+#    _eth=`ip a |grep "10.1.1" -B2|head -1 |awk -F: '{print $2}'`
+#    _num=`iptables -nL --line-number|grep REJECT|awk '{print $1}'|head -1`
+#    iptables -I INPUT $_num -i $_eth -p tcp -m state --state NEW -m tcp --dport 10050 -j ACCEPT
+#    /etc/init.d/iptables save
+#    iptables -nL --line-numbers |grep 10050
+#fi
 
-chkconfig zabbix-agent on
 service zabbix-agent start
-
 echo "`date +%F_%T` common/zabbix " >> /tmp/ezdpl.log
