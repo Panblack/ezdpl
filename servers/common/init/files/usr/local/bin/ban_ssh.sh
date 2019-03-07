70e8e76dad635c8c599d5a15fed593dcb5992975
panblack <panblack@126.com>
2019-03-07 18:05:47 +0800

    ban_ssh.sh: big fix

--- servers/common/init/files/usr/local/bin/ban_ssh.sh
+++ servers/common/init/files/usr/local/bin/ban_ssh.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
-cat /var/log/secure|awk '/Failed password/{print $(NF-3)}' |sort|uniq -c|sort -n  > /var/log/black.list
-cat /var/log/secure|awk '/Failed publickey/{print $(NF-5)}'|sort|uniq -c|sort -n >> /var/log/black.list
+cat /var/log/secure|awk '/Failed password/{print $(NF-3)}'  >  /dev/shm/black.lst
+cat /var/log/secure|awk '/Failed publickey/{print $(NF-5)}' >> /dev/shm/black.lst
+cat /var/log/secure|awk '/Invalid user/{print $(NF-2)}'     >> /dev/shm/black.lst
+cat /var/log/secure|awk '/maximum authentication attempts exceeded for/{print $(NF-4)}' >> /dev/shm/black.lst
+sort /dev/shm/black.lst|uniq -c|sort -h > /var/log/black.list
 if [[ `date +%d` = 01 ]] && [[ `date +%H` = 01  ]] ;then
   sed -i /"deny"/d /etc/hosts.deny 
 fi
 
-IFS="
-"
+IFS=$'\n'
 for i in `cat  /var/log/black.list`; do
   NUM=`echo $i|awk '{print $1}'`
   IP=`echo $i |awk '{print $2}'`
