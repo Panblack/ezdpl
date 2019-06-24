90dc190f3cab7a51f340c7049258c533a15f0ccd
Panblack <panblack@126.com>
2019-06-24 23:49:09 +0800

    zz_jdk.sh removed from websrv/javasrv

--- servers/websrv/javasrv/files/etc/profile.d/zz_jdk.sh
+++
@@ -1,6 +0,0 @@
-export JAVA_HOME=/opt/jdk
-export JRE_HOME=$JAVA_HOME/jre
-export CLASSPATH=$CLASSPATH:.:$JAVA_HOME/lib:$JAVA_HOME/jre/lib
-export MAVEN_HOME=/opt/maven
-export M2_HOME=$MAVEN_HOME
-export PATH=$PATH:$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$MAVEN_HOME/bin
