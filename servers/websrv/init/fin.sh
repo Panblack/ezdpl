cbd55b8891cfbf0b31488869c837f93ae944e61d
panblack <panblack@126.com>
2019-06-25 11:08:25 +0800

    websrv/init/fin.sh:skip nfs, added empty zz_jdk.sh

--- servers/websrv/init/fin.sh
+++ servers/websrv/init/fin.sh
@@ -20,97 +20,96 @@ echo "$_LOCAL_DIRS_FOR_HTML created."
 echo
 
 # prepare for nfs shares
-if [[ $_USE_NFS = 0 ]]; then
-    exit 0
-fi
+if [[ $_USE_NFS = 1 ]]; then
 
-# install nfs-utils
-killall -9 yum
-yum clean all
-yum -y install nfs-utils
-yum -y update  nfs-utils
-
-case $_RELEASE in
-    CENTOS6)
-	chkconfig nfs off
-	chkconfig nfslock off
-	chkconfig rpcbind off
-	chkconfig rpcgssd off
-	chkconfig rpcidmapd off
-	chkconfig rpcsvcgssd off
-	service nfs stop
-	service nfslock stop
-	service rpcbind stop
-	service rpcgssd stop
-	service rpcidmapd stop
-	service rpcsvcgssd stop
-	;;
-    CENTOS7)
-	systemctl disable nfs.service
-	systemctl disable nfslock.service
-	systemctl disable nfs-mountd.service
-	systemctl disable rpcbind.service
-	systemctl disable rpcgssd.service
-	systemctl disable rpcidmapd.service
-	systemctl disable rpcsvcgssd.service
-        systemctl disable rpc-gssd.service
-        systemctl disable rpc-rquotad.service
-        systemctl disable rpc-statd-notify.service
-        systemctl disable rpc-statd.service
-	systemctl stop nfs.service
-	systemctl stop nfslock.service
-	systemctl stop nfs-mountd.service
-	systemctl stop rpcbind.service
-	systemctl stop rpcgssd.service
-	systemctl stop rpcidmapd.service
-	systemctl stop rpcsvcgssd.service
-        systemctl stop rpc-gssd.service
-        systemctl stop rpc-rquotad.service
-        systemctl stop rpc-statd-notify.service
-        systemctl stop rpc-statd.service
-	;;
-    UBUNTU)
-	;;
-esac
-
-echo
-echo "nfs-utils (client) installed."
-echo
-
-# release nfs4 mounts if present
-for x in `mount -t nfs4|awk '{print $3}'`; do
-    fuser -km $x
-done
-
-if ! umount -af -t nfs4 ;then
+  # install nfs-utils
+  killall -9 yum
+  yum clean all
+  yum -y install nfs-utils
+  yum -y update  nfs-utils
+  
+  case $_RELEASE in
+      CENTOS6)
+  	chkconfig nfs off
+  	chkconfig nfslock off
+  	chkconfig rpcbind off
+  	chkconfig rpcgssd off
+  	chkconfig rpcidmapd off
+  	chkconfig rpcsvcgssd off
+  	service nfs stop
+  	service nfslock stop
+  	service rpcbind stop
+  	service rpcgssd stop
+  	service rpcidmapd stop
+  	service rpcsvcgssd stop
+  	;;
+      CENTOS7)
+  	systemctl disable nfs.service
+  	systemctl disable nfslock.service
+  	systemctl disable nfs-mountd.service
+  	systemctl disable rpcbind.service
+  	systemctl disable rpcgssd.service
+  	systemctl disable rpcidmapd.service
+  	systemctl disable rpcsvcgssd.service
+          systemctl disable rpc-gssd.service
+          systemctl disable rpc-rquotad.service
+          systemctl disable rpc-statd-notify.service
+          systemctl disable rpc-statd.service
+  	systemctl stop nfs.service
+  	systemctl stop nfslock.service
+  	systemctl stop nfs-mountd.service
+  	systemctl stop rpcbind.service
+  	systemctl stop rpcgssd.service
+  	systemctl stop rpcidmapd.service
+  	systemctl stop rpcsvcgssd.service
+          systemctl stop rpc-gssd.service
+          systemctl stop rpc-rquotad.service
+          systemctl stop rpc-statd-notify.service
+          systemctl stop rpc-statd.service
+  	;;
+      UBUNTU)
+  	;;
+  esac
+  
   echo
-  echo "Failed to 'umount -af -t nfs4'. EXIT!"
+  echo "nfs-utils (client) installed."
   echo
-  exit 1
-fi
-
-# backup /data/webShare if present
-if [[ -d /data/webShare ]]; then
-  cd /data && /bin/mv -f webShare* $_backup_dir
-fi
-
-mkdir -p $_LOCAL_DIRS_FOR_NFS
-
-# configure nfs share
-if [[ -n $_WEBSRV_FSTAB ]]; then
-  /bin/cp -p /etc/fstab $_backup_dir
-  sed -i '/nfs4/d' /etc/fstab
-  echo "$_WEBSRV_FSTAB" >> /etc/fstab
-  if mount -a -t nfs4; then
-    echo "nfs4 mounts ok"
+  
+  # release nfs4 mounts if present
+  for x in `mount -t nfs4|awk '{print $3}'`; do
+      fuser -km $x
+  done
+  
+  if ! umount -af -t nfs4 ;then
     echo
-  else
-    echo "Failed to 'mount -a -t nfs4'. EXIT!"
+    echo "Failed to 'umount -af -t nfs4'. EXIT!"
     echo
     exit 1
   fi
+  
+  # backup /data/webShare if present
+  if [[ -d /data/webShare ]]; then
+    cd /data && /bin/mv -f webShare* $_backup_dir
+  fi
+  
+  mkdir -p $_LOCAL_DIRS_FOR_NFS
+  
+  # configure nfs share
+  if [[ -n $_WEBSRV_FSTAB ]]; then
+    /bin/cp -p /etc/fstab $_backup_dir
+    sed -i '/nfs4/d' /etc/fstab
+    echo "$_WEBSRV_FSTAB" >> /etc/fstab
+    if mount -a -t nfs4; then
+      echo "nfs4 mounts ok"
+      echo
+    else
+      echo "Failed to 'mount -a -t nfs4'. EXIT!"
+      echo
+      exit 1
+    fi
+  fi
+  
 fi
-
 
 # Get nginx ready
 case $_RELEASE in
